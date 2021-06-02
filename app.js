3f3be066c4c25253840720721b996167c0efa07b
Olivia Boynton <liv.madrid14@gmail.com>
2021-06-02 09:47:16 -0700

    Create html form, Add/Delete functions on JS, edit css styles and classes

---
+++ app.js
@@ -0,0 +1,70 @@
+// Create selector constants
+const toDoInput = document.querySelector('.todo-input');
+const toDoButton = document.querySelector('.todo-button');
+const toDoList = document.querySelector('.todo-list');
+
+
+// event listeners 
+toDoButton.addEventListener('click', addToDo);
+toDoList.addEventListener('click', deleteTask);
+
+// functions
+function addToDo (event) {
+    // keeps 
+    event.preventDefault();
+
+    //  creates new div for newly added tasks
+    const toDoDiv = document.createElement('div');
+    toDoDiv.classList.add('todo');
+
+    // creates new to do item/text within toDoDiv--- creates list
+    const newToDo = document.createElement('li');
+    newToDo.innerText= toDoInput.value;
+    newToDo.classList.add('to-do-item');
+    toDoDiv.appendChild(newToDo);
+
+    // creates checked mark for task completed
+    const checkMarkButton = document.createElement('button');
+    checkMarkButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkMarkButton.classList.add('checkMark-btn');
+    toDoDiv.appendChild(checkMarkButton);
+
+    // garbage button for deleted tasks
+    const garbageButton = document.createElement('button');
+    garbageButton.innerHTML = '<i class="fas fa-trash"></i>';
+    garbageButton.classList.add('garbage-btn');
+    toDoDiv.appendChild(garbageButton);
+
+    // appends tasks in the newly created div to the to do list
+    toDoList.appendChild(toDoDiv);
+
+    // clear input.value 
+    toDoInput.value="";
+
+}
+
+
+function deleteTask(e) {
+    console.log(e.target);
+    const item = e.target;
+
+    // delete task
+    if(item.classList[0] === 'garbage-btn') {
+        const toDo = item.parentElement;
+        // add animation 
+        toDo.classList.add('cascade');
+        toDo.addEventListener('transitionend', function() {
+            toDo.remove();
+        })
+     
+    }
+
+    // check task complete 
+    if(item.classList[0] === 'checkMark-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('finito')
+    }
+}
+// Create an empty array to store tasks 
+
+// let toDoTasks = [];
